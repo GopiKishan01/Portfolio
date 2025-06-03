85aea3b0272176470ba0b3ea4087e3c2e23a666f
GOPI KISHAN ARYA <113343219+GopiKishan01@users.noreply.github.com>
2025-06-03 17:29:25 +0530

    Update data.php

--- data.php
+++ data.php
@@ -7,7 +7,7 @@ $db = "data";
 $conn = mysqli_connect($server, $user, $password, $db);
 
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+     header("Location: error.html");
 }
 
 $name = $_POST['name'];
